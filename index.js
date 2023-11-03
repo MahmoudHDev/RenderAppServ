445f2192352f31dcfdb7bbde2e7ab165075445e2
Mahmoud Hashim <mahmoud.ios.dev@gmail.com>
2023-11-03 21:17:25 +0200

    adding the rest files

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get('/', (req,res)=> { 
+
+
+    res.send("<h1> hello, World </h1>");
+});
+
+app.listen(port, () => { 
+    console.log("Server started listening");
+})
