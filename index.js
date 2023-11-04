1b12ca24ebed5126c90dd0c5b95ebc84da4db939
Mahmoud Hashim <mahmoud.ios.dev@gmail.com>
2023-11-04 13:59:47 +0200

    adding style sheet

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import path from'path';
+
 
 const app = express();
-const port = 3000;
+const port = 3000
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static('public'));
 
 app.get('/', (req,res)=> { 
     res.sendFile(__dirname + '/index.html')
