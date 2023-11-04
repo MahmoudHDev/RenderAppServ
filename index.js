acacf11646c63c93aad9382d466a066ecd427df2
Mahmoud Hashim <mahmoud.ios.dev@gmail.com>
2023-11-04 13:39:22 +0200

    correcting some errors

--- index.js
+++ index.js
@@ -7,12 +7,10 @@ const app = express();
 const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.get('/', (req,res)=> { 
-
-
+app.get(__dirname + '/', (req,res)=> { 
     res.send("<h1> hello, World </h1>");
 });
 
 app.listen(port, () => { 
     console.log("Server started listening");
-})
+});
